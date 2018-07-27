031cd4c8cd60ec0c08b807d5ef813f08454736e9
Naz <naz@monzo.com>
2018-07-27 17:45:32 +0100

    Update images and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import github from "../img/github-icon.svg";
+import logo from "../img/whale.png";
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Naz Malik" style={{ width: "40px" }} />
           </figure>
         </Link>
       </div>
@@ -25,7 +25,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/nazwhale"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -36,6 +36,6 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
